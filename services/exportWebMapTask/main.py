063be3facd5207b0bf703ea1fc662831af5f2f34
Roy Yali <ryali93@gmail.com>
2018-12-27 16:29:22 -0500

    servicio de identificacion de peligros creado

--- services/exportWebMapTask/main.py
+++ services/exportWebMapTask/main.py
@@ -13,7 +13,7 @@ import time
 
 arcpy.env.overwriteOutput = True
 
-# directory = r'D:\RYali\TDR3\3Product')
+# directory = r'D:\RYali\TDR3\3Product
 directory = r'C:\plantillas_dgar'
 
 class exportWebMapTask(object):
@@ -48,6 +48,14 @@ class exportWebMapTask(object):
         outputJson = json.dumps(dicc)
         e = dicc["mapOptions"]["extent"]
         self.xy = [(e["xmax"] + e["xmin"])/2, (e["ymax"] + e["ymin"])/2]
+
+        array = arcpy.Array([arcpy.Point(e["xmax"], e["ymax"]),
+        arcpy.Point(e["xmax"], e["ymin"]),
+        arcpy.Point(e["xmin"], e["ymin"]),
+        arcpy.Point(e["xmin"], e["ymax"])
+        ])
+        self.polygon = arcpy.Polygon(array, arcpy.SpatialReference(102100))
+
         return outputJson
 
     def extractTitleMap(self):
@@ -67,6 +75,12 @@ class exportWebMapTask(object):
         zona = [x[0] for x in arcpy.da.SearchCursor(zonaSelect, ["Zona"])][0]
         return zona
 
+    def extractTipoPeligro(self):
+        peligros = arcpy.mapping.ListLayers(self.mxd, "Poligonos", self.df)[0]
+        peligrosSelect = arcpy.SelectLayerByLocation_management(peligros, 'INTERSECT', self.polygon, '#', 'NEW_SELECTION', 'NOT_INVERT')
+        peligro = [int(x[0]) for x in arcpy.da.SearchCursor(peligros, ["TIPO"])]
+        return peligro
+
     def changeGridZone(self, zone):
         if zone == u'Z-17':
             self.df.spatialReference = arcpy.SpatialReference(32717)
@@ -75,6 +89,36 @@ class exportWebMapTask(object):
         elif zone == u'Z-19':
             self.df.spatialReference = arcpy.SpatialReference(32719)
         arcpy.RefreshActiveView()
+
+    def updateScale(self, df, lyrLeyenda):
+        # df.extent = lyrLeyenda.getSelectedExtent()
+        newExtent = df.extent
+        newExtent.XMin, newExtent.YMin = -0.025, -0.0245
+        newExtent.XMax, newExtent.YMax = 0.036, 0.007
+        df.extent = newExtent
+        # df.scale = df.scale*1.2
+        arcpy.RefreshActiveView()
+
+    def selectTipoPeligro(self, listTipo):
+        df = arcpy.mapping.ListDataFrames(self.mxd, "Leyenda")[0]
+        ptLeyenda = arcpy.mapping.ListLayers(self.mxd, "GPT_HM_Leyenda")[0]
+        arcpy.DeleteRows_management(ptLeyenda)
+        xini = 0
+        yini = 0
+        rowx = 0
+        rowy = 0
+        cursor = arcpy.da.InsertCursor(ptLeyenda, ["SHAPE@X", "SHAPE@Y", "TIPO"])
+        for i in range(len(listTipo)):
+            cursor.insertRow([xini + rowx, yini + rowy, listTipo[i]])
+            if len(listTipo) > 1:
+                rowy = rowy - 0.004
+                if i == 7:
+                    rowy = 0
+                    rowx = 0.032
+        del cursor
+        arcpy.RefreshActiveView()
+
+        self.updateScale(df, ptLeyenda)
 
     def selectTemplate(self):
         zona = self.extractZone()
@@ -90,9 +134,13 @@ class exportWebMapTask(object):
         self.mxd = proyecto.mapDocument
         self.df = arcpy.mapping.ListDataFrames(self.mxd)[2]
 
+        peligro = self.extractTipoPeligro()
+        peligro = list(set(peligro))
+
+        self.selectTipoPeligro(peligro)
         self.changeGridZone(zona)
 
-        update_layer = arcpy.mapping.ListLayers(self.mxd, 'Peligros - lineas', self.df)[0]
+        update_layer = arcpy.mapping.ListLayers(self.mxd, 'Peligros - Lineas', self.df)[0]
         source_layer = arcpy.mapping.Layer(self.lineLyr)
         arcpy.mapping.UpdateLayer(self.df, update_layer, source_layer, symbology_only = True)
 
